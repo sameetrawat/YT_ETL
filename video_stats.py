e77f9e75439ea2096095edc24ae4728b8354a8b1
sameetrawat <126938682+sameetrawat@users.noreply.github.com>
2026-02-04 18:18:57 +0100

    added video id extraction logic

--- video_stats.py
+++ video_stats.py
@@ -36,3 +36,92 @@ def get_playlist_id():
 if __name__=="__main__":
     get_playlist_id()
 
+
+import requests
+import json
+
+import os
+from dotenv import load_dotenv
+
+load_dotenv(dotenv_path="./.env")
+
+
+API_KEY =os.getenv("API_KEY")
+CHANNEL_HANDLES= "MrBeast"
+maxResults=50
+
+def get_playlist_id():
+    try:
+        
+        url = f"https://youtube.googleapis.com/youtube/v3/channels?part=contentDetails&forHandle={CHANNEL_HANDLES}&key={API_KEY}"
+
+        response = requests.get(url)
+
+        response.raise_for_status()
+
+        data= response.json()
+
+        #print(json.dumps(data,indent=4))
+
+        channel_items=data["items"][0]
+
+        channel_playlistId=channel_items["contentDetails"]["relatedPlaylists"]["uploads"]
+        print(channel_playlistId)
+    
+        return channel_playlistId
+
+    except requests.exceptions.RequestException as e:
+        raise e
+    
+
+
+ 
+    
+    
+    
+
+def get_video_ids(playlistId):
+
+    video_ids=[] 
+
+    pageToken= None
+
+    base_url =f"https://youtube.googleapis.com/youtube/v3/playlistItems?part=contentDetails&maxResults={maxResults}&playlistId={playlistId}&key={API_KEY}" 
+
+    try:
+
+        while True: 
+            
+            url= base_url 
+
+            if pageToken:
+                url += f"&pageToken={pageToken}"
+                 
+            response = requests.get(url) 
+
+            response.raise_for_status()
+                     
+            data= response.json()
+                    
+
+            for item in data.get('items',[]):
+                video_id = item['contentDetails']['videoId']
+                video_ids.append(video_id)
+
+
+            pageToken = data.get('nextPageToken')
+
+            if not pageToken:
+                break 
+                    
+        return video_ids
+                  
+    except requests.exceptions.RequestException as e:
+            raise e 
+    
+
+if __name__=="__main__":
+    playlistId = get_playlist_id() 
+    (get_video_ids(playlistId))
+
+
